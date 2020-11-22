24203dabaf7f9946539ec5dca23016d03e14da16
Nicky <1135022744@qq.com>
2020-11-22 21:24:29 +0800

    '记住密码'

--- src/views/Login/login.vue
+++ src/views/Login/login.vue
@@ -6,6 +6,7 @@
     <div class="login_options">
       <el-input class="mb20" v-model="accont" clearable placeholder="账号" />
       <el-input class="mb20" v-model="passWord" show-password clearable placeholder="密码"/>
+      <el-checkbox class="mb20" v-model="checked">记住密码</el-checkbox>
       <span class="custom_btn custom_shadow bg_orange text_white" @click="_login">登 录</span>
     </div>
   </div>
@@ -29,8 +30,12 @@ export default class Login extends Vue {
   // @Prop() data!: any;
   private accont: any = "";
   private passWord: any = "";
+  private checked = false;
 
-  // mounted() {}
+  mounted() {
+    // !TODO 记住密码待优化
+    this.getCookie();
+  }
 
   _login() {
     const postData = {
@@ -39,6 +44,17 @@ export default class Login extends Vue {
     };
     console.log("login", postData);
     login(postData).then((res: any) => {
+      //判断复选框是否被勾选 勾选则调用配置cookie方法
+      if (this.checked == true) {
+        console.log("checked == true");
+        //传入账号名，密码，和保存天数3个参数
+        this.setCookie(this.accont, this.passWord, 365);
+      }else {
+        console.log("清空Cookie");
+        //清空Cookie
+        this.clearCookie();
+      }
+
       console.log('loginres =>', res);
       // this.$store.dispatch('userInfo', res.data);
       sessionStorage.setItem("userInfo", JSON.stringify(res.data));
@@ -46,6 +62,37 @@ export default class Login extends Vue {
     }).catch((err: any) => {
       console.log('loginerr =>', err);
     });
+  }
+
+  //设置cookie
+  setCookie(name: any, pwd: any, exdays: any) {
+    const exdate: any = new Date(); //获取时间
+    exdate.setTime(exdate.getTime() + 24 * 60 * 60 * 1000 * exdays); //保存的天数
+    //字符串拼接cookie
+    window.document.cookie = "accont" + "=" + name + ";path=/;expires=" + exdate.toGMTString();
+    window.document.cookie = "userPwd" + "=" + pwd + ";path=/;expires=" + exdate.toGMTString();
+  }
+
+  //读取cookie
+  getCookie() {
+    if (document.cookie.length > 0) {
+      const arr = document.cookie.split('; '); //这里显示的格式需要切割一下自己可输出看下
+      for (let i = 0; i < arr.length; i++) {
+        this.checked = true;
+        const arr2 = arr[i].split('='); //再次切割
+        //判断查找相对应的值
+        if (arr2[0] == 'accont') {
+          this.accont = arr2[1]; //保存到保存数据的地方
+        } else if (arr2[0] == 'userPwd') {
+          this.passWord = arr2[1];
+        }
+      }
+    }
+  }
+
+  //清除cookie
+  clearCookie() {
+    this.setCookie(null, "", -1); //修改2值都为空，天数为负1天就好了
   }
 
   // @Watch("")
--- src/views/task/compontent/product.vue
+++ src/views/task/compontent/product.vue
@@ -12,28 +12,28 @@
 
         <div class="flex_column price_wrap">
           <div class="flex_row">
-            <el-form-item class="price_input inline_block mr10" :prop="'data' + '.' +index + '.num1First'" :rules="rules.num1First">
+            <el-form-item class="price_input inline_block mr10" :prop="`data.${index}.num1First`" :rules="rules.num1First">
               <el-input v-model="item.num1First" @change="countChange(item)" clearable placeholder="盘点数量" />
             </el-form-item>
-            <el-form-item class="price_input inline_block" :prop="'data' + '.' +index + '.num1Two'" :rules="rules.num1Two">
+            <el-form-item class="price_input inline_block" :prop="`data.${index}.num1Two`" :rules="rules.num1Two">
               <el-input v-model="item.num1Two" @change="countChange(item)" clearable placeholder="盘点数量" />
             </el-form-item>
             <span class="price_unit">{{ item.unit1 }}({{ item.unit1Coefficient }}克)</span>
           </div>
           <div class="flex_row">
-            <el-form-item class="price_input inline_block mr10" :prop="'data' + '.' +index + '.num2First'" :rules="rules.num2First">
+            <el-form-item class="price_input inline_block mr10" :prop="`data.${index}.num2First`" :rules="rules.num2First">
               <el-input v-model="item.num2First" @change="countChange(item)" clearable placeholder="盘点数量" />
             </el-form-item>
-            <el-form-item class="price_input inline_block" :prop="'data' + '.' +index + '.num2Two'" :rules="rules.num2Two">
+            <el-form-item class="price_input inline_block" :prop="`data.${index}.num2Two`" :rules="rules.num2Two">
               <el-input v-model="item.num2Two" @change="countChange(item)" clearable placeholder="盘点数量" />
             </el-form-item>
             <span class="price_unit">{{ item.unit2 }}({{ item.unit2Coefficient }}克)</span>
           </div>
           <div class="flex_row" v-if="isEmptyUnit(item)">
-            <el-form-item class="price_input inline_block mr10" :prop="'data' + '.' +index + '.num3First'" :rules="rules.num3First">
+            <el-form-item class="price_input inline_block mr10" :prop="`data.${index}.num3First`" :rules="rules.num3First">
               <el-input v-model="item.num3First" @change="countChange(item)" clearable placeholder="盘点数量" />
             </el-form-item>
-            <el-form-item class="price_input inline_block" :prop="'data' + '.' +index + '.num3Two'" :rules="rules.num3Two">
+            <el-form-item class="price_input inline_block" :prop="`data.${index}.num3Two`" :rules="rules.num3Two">
               <el-input v-model="item.num3Two" @change="countChange(item)" clearable placeholder="盘点数量" />
             </el-form-item>
             <span class="price_unit">{{ item.unit3 }}({{ item.unit3Coefficient }}克)</span>
